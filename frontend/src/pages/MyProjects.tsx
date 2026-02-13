e784c3e077c7be45d92b74157bdb9bb0c670b135
Dhairya Tiwari <Dhairyatiwari7@users.noreply.github.com>
2026-02-13 21:58:59 +0530

    fized frontend

--- frontend/src/pages/MyProjects.tsx
+++ frontend/src/pages/MyProjects.tsx
@@ -49,7 +49,7 @@ const MyProjects = () => {
         navigate('/')
         toast.message('Please Login to view your message')
       }
-  }, [session.user]);
+  }, [session,isPending]);
 
   return (
     <div className="px-4 md:px-16 lg:px-24 xl:px-32">
